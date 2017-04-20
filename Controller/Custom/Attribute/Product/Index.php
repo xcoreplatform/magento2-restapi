2349704424120892551c38e1ab1149b8cd29ea0b
Henri Nijborg <h.nijborg@linq5.nl>
2017-04-20 15:45:28 +0200

    Scaffolded pages

---
+++ Controller/Custom/Attribute/Product/Index.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Dealer4dealer\Xcore\Controller\Custom\Attribute\Product;
+
+use Magento\Framework\App\Action\Action;
+use Magento\Framework\App\Action\Context;
+use Magento\Framework\View\Result\PageFactory;
+
+class Index extends Action
+{
+    protected $resultPageFactory;
+
+    public function __construct(Context $context, PageFactory $resultPageFactory)
+    {
+        $this->resultPageFactory = $resultPageFactory;
+        parent::__construct($context);
+    }
+
+    public function execute()
+    {
+        $resultPage = $this->resultPageFactory->create();
+        return $resultPage;
+    }
+}
