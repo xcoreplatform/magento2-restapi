fa3491e640540682d9393febf7361c03172729e7
Eelco <e.dam@linq5.nl>
2017-08-08 10:44:47 +0200

    Added xCore custom attributes on customers

--- Plugin/Magento/Customer/Api/Data/CustomerInterface.php
+++ Plugin/Magento/Customer/Api/Data/CustomerInterface.php
@@ -22,38 +22,38 @@ class CustomerInterface
         $this->extensionFactory = $extensionFactory;
     }
 
-//    /**
-//     * @api
-//     * @param \Magento\Customer\Api\Data\CustomerInterface $subject
-//     * @param $result
-//     * @return \Magento\Customer\Api\Data\CustomerExtensionInterface
-//     */
-//    public function afterGetExtensionAttributes(
-//        \Magento\Customer\Api\Data\CustomerInterface $subject,
-//        $result
-//    ) {
-//
-//        if ($result === null) {
-//            $result = $this->extensionFactory->create();
-//        }
-//
-//        // Get the custom attributes
-//        $repo = $this->objectManager->get('Dealer4dealer\Xcore\Model\CustomAttributeRepository');
-//        $customCustomerAttributes = $repo->getListByType('customer');
-//
-//        // Get the actual value of the custom attributes
-//        $customAttributes = [];
-//        foreach($customCustomerAttributes as $customCustomerAttribute) {
-//            $key = $customCustomerAttribute['to'];
-//            $value = $subject->getCustomAttribute([$customCustomerAttribute['from']]);
-//            if(!$value)
-//                $value = $subject->{"get".$customCustomerAttribute['from']}();
-//            $customAttributes[] = ['key' => $key, 'value' => $value];
-//        }
-//
-//        // Set the Extension Attributes for Xcore Custom Attributes
-//        $result->setXcoreCustomAttributes($customAttributes);
-//
-//        return $result;
-//    }
+    /**
+     * @api
+     * @param \Magento\Customer\Api\Data\CustomerInterface $subject
+     * @param $result
+     * @return \Magento\Customer\Api\Data\CustomerExtensionInterface
+     */
+    public function afterGetExtensionAttributes(
+        \Magento\Customer\Api\Data\CustomerInterface $subject,
+        $result
+    ) {
+
+        if ($result === null) {
+            $result = $this->extensionFactory->create();
+        }
+
+        // Get the custom attributes
+        $repo = $this->objectManager->get('Dealer4dealer\Xcore\Model\CustomAttributeRepository');
+        $customCustomerAttributes = $repo->getListByType('customer');
+
+        // Get the actual value of the custom attributes
+        $customAttributes = [];
+        foreach($customCustomerAttributes as $customCustomerAttribute) {
+            $key = $customCustomerAttribute['to'];
+            $value = $subject->getCustomAttribute($customCustomerAttribute['from']);
+            if(!$value)
+                $value = $subject->{"get".$customCustomerAttribute['from']}();
+            $customAttributes[] = ['key' => $key, 'value' => $value];
+        }
+
+        // Set the Extension Attributes for Xcore Custom Attributes
+        $result->setXcoreCustomAttributes($customAttributes);
+        
+        return $result;
+    }
 }
